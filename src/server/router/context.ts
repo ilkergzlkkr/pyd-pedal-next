07131d9c123eda50393a99c1a558ff77b5f8cbde
ilkergzlkkr <guzelkokarilker@gmail.com>
2022-09-11 16:14:26 +0300

    move create protected router func to the context

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -53,3 +53,22 @@ export const createContext = async (
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
 
 export const createRouter = () => trpc.router<Context>();
+
+/**
+ * Creates a tRPC router that asserts all queries and mutations are from an authorized user. Will throw an unauthorized error if a user is not signed in.
+ */
+export const createProtectedRouter = () => {
+  return createRouter().middleware(({ ctx, next }) => {
+    if (!ctx.session || !ctx.session.user) {
+      throw new trpc.TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next({
+      ctx: {
+        ...ctx,
+        // infers that `session` is non-nullable to downstream resolvers
+        session: { ...ctx.session, user: ctx.session.user },
+      },
+    });
+  });
+}
+
