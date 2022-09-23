fac49c515e441d617676d384e247bfc243e083ac
ilkergzlkkr <guzelkokarilker@gmail.com>
2022-09-23 19:35:45 +0300

    QoL: websocket-reconnect logic

--- src/server/router/pypedal.ts
+++ src/server/router/pypedal.ts
@@ -13,51 +13,20 @@ import {
   StatusResponse,
   youtubeVideoRegex,
   videoUrlValidator,
-  serverResponseValidator,
   ServerRequest,
 } from "../../utils/pypedal";
+import { createNewWebSocket } from "../../utils/pypedalWebsockets";
 
 const DEBUG = process.env.NODE_ENV === "development";
 
 // create a global event emitter (could be replaced by redis, etc)
 const ee = new EventEmitter();
-const wss = new WebSocket(process.env.PEDAL_WEBSOCKET_URL || "/ws", {
-  headers: {
-    Authorization: process.env.PEDAL_WEBSOCKET_SECRET,
-  },
-});
+const wss = createNewWebSocket(ee);
 
-wss.onerror = (event) => {
-  console.log("wss.onerror", event);
-};
-
-wss.onmessage = (event) => {
-  if (typeof event.data !== "string") {
-    console.error("Received non-string data");
-    return;
-  }
-
-  const payload = JSON.parse(event.data.toString());
-  // safe parse data from server
-  const parsed = serverResponseValidator.safeParse(payload);
-  if (!parsed.success) {
-    console.error("Invalid data from server", parsed.error);
-    return;
-  }
-
-  const { op, data } = parsed.data;
-  if (op === "STATUS") {
-    onServerStatusResponse(data);
-  } else if (op === "INTERNAL_ERROR") {
-    // code 2 -> validation error
-    console.error("Server error", data);
-    if (data.disconnected) wss.close(undefined, data.message);
-  }
-};
 // mapping of [id][board_name][board_type] -> status
 // const boardLastStatusCache = ...;
 
-const onServerStatusResponse = (response: StatusResponse) => {
+export const onServerStatusResponse = (response: StatusResponse) => {
   if (DEBUG) console.log("onServerStatusResponse <<", response);
   // update cache
   // ...
